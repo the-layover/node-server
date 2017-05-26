5fc06f7c6dc6b461b9926b96d57f2b4d9d5acac9
Jason Kuo <jasonkuo@Jasons-MacBook-Pro.local>
2017-05-25 22:04:15 -0700

    created user schema

---
+++ db/user/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  username: {type: String, unique: true},
+  email: {type: String, unique: true},
+  password: String,
+});
+
+const Users = mongoose.model('Users', userSchema);
+
+module.exports = Users;
